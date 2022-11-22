1d39a54804bbc53098e8564ca5597a0fa8c5038f
Bruno <brunolimadevelopment@gmail.com>
2022-11-22 00:08:51 -0300

    branch footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,9 +4,6 @@ import { Wrapper } from './style'
 
 const Footer = () => (
     <Wrapper>
-        <p>
-            Do you want to get updates and news from Cake it? Contact us on the adress below.
-        </p>
         <p>CakeIT! | Cake Boulevard 10 | Fiction Land | cakeit@cakeit.fiction</p>
     </Wrapper>
 )
