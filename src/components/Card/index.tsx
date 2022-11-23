592bea7a082559a67dfcb5b523c925a5cdee7cbe
Bruno <brunolimadevelopment@gmail.com>
2022-11-23 02:39:49 -0300

    front

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,22 +1,60 @@
 import { Article } from '../Card/style'
 import { useFetch } from '../../hooks/useFetch'
+import { Wrapper } from "./style"
+
 
 export default function Card(){
   
+  function removeTags(str:string):boolean|string {
+    if ((str===null) || (str===''))
+      return false;
+    else
+      str = str.toString();
+          
+    // Regular expression to identify HTML tags in 
+    // the input string. Replacing the identified 
+    // HTML tag with a null string.
+    return str.replace( /(<([^>]+)>)/ig, '');
+  }
+
+  function dateFormat(date:string):string {
+    const today = new Date(date);
+    const yyyy = today.getFullYear();
+    let mm:string|number = today.getMonth() + 1; // Months start at 0!
+    let dd:string|number = today.getDate();
+    let hh:string|number = today.getHours();
+    let m:string|number = today.getMinutes();
+    let ss:string|number = today.getSeconds();
+
+    if (dd < 10) dd = '0' + dd;
+    if (mm < 10) mm = '0' + mm;
+    if (m <  10)  m = '0' + m;
+    if (ss < 10) ss = '0' + ss;
+
+    const date_formated = dd + '/' + mm + '/' + yyyy + ' às ' + hh + ':' + m + ':' + ss;
+
+    return date_formated;
+  }
+
+
+
+  
+
   const {data} = useFetch('https://blog.apiki.com/wp-json/wp/v2/posts/?per_page=10')
-  console.log(data)
+  //console.log(data)
     return(
-      <div>
-        {/* {data?.map( (e:any) => {
-          // console.log(e)
-            return(
-              <Article>
-                <h3>teste</h3>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet provident deserunt eligendi mollitia.</p>
-              </Article>
-            )
-          })
-        } */}
-      </div>
+      <Wrapper>
+        { data.map((e:any, index:number) => {
+          console.log(e)
+          return (
+            <Article key={index}>
+              <h3>{e.title.rendered}</h3>
+              <time>Postado em: {dateFormat(e.date)}</time>
+              <p>{removeTags(e.excerpt.rendered)}</p>
+              <a href={e.link}>Saiba mais</a>
+            </Article>
+          )})
+        }
+      </Wrapper>
     )
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,14 @@
 import React from 'react'
-//import { Link } from 'gatsby'
-import Navigation from '../Nav/index'
-//import Logo from '../../images/logo.svg'
-//import { useMenuQuery } from '../../hooks/useMenuQuery'
+//import Navigation from '../Nav/index'
+import logo from '../../assets/images/logo-apiki.webp'
 import { Wrapper, Content } from '../Header/style'
 
 const Header = () => {    
-  //const { site, menu } = useMenuQuery()
   
   return (
     <Wrapper>
       <Content>
-          <a href="#">
-              {/* <img src={Logo} alt={site.siteMetadata.title} /> */}
-          </a>
+        <img src={logo} alt="" />
           {/* <Navigation menu={menu.menuItems.nodes} />  */}
       </Content>
     </Wrapper>
